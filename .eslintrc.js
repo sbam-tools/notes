7337cfa1d9695f1c1316ef476c710f6a3c24ba80
Nicola Racco <nicola@mikamai.com>
2022-04-13 15:05:32 +0200

    deps and linting

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    jest: true,
+    node: true,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'jest',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: '2018',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:jest/recommended',
+  ],
+  ignorePatterns: ['*.js', '*.d.ts', 'node_modules/', '*.generated.ts'],
+};

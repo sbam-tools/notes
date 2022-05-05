4f787ed5c9a10ad3b50b5b9a14b46ecf6cad7c8f
Nicola Racco <nicola@mikamai.com>
2022-05-05 21:56:22 +0200

    add license

--- lambda/services/cloudfront-service.ts
+++ lambda/services/cloudfront-service.ts
@@ -1,3 +1,17 @@
+// Copyright (C) 2022 Nicola Racco
+//
+// SBAM Notes is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SBAM Notes is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with SBAM Notes. If not, see <http://www.gnu.org/licenses/>.
 import { inject, registry, singleton } from 'tsyringe';
 import { CloudFrontClient, CreateInvalidationCommand } from '@aws-sdk/client-cloudfront';
 
